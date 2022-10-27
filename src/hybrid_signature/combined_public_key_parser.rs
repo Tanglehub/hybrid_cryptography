ae7948744156aa59fcc7998e5220460ab10ba837
Noman Nasir Minhas <SP17-BSE-010@ISBSTUDENT.COMSATS.EDU.PK>
2022-10-27 12:37:37 +0500

    Update combined_public_key_parser.rs

--- src/hybrid_signature/combined_public_key_parser.rs
+++ src/hybrid_signature/combined_public_key_parser.rs
@@ -16,40 +16,35 @@ pub fn parse_combined_public_key(purpose: AlgorithmPurpose, combined_public_key:
     while idx < combined_public_key.len() {
         let scheme_id = (combined_public_key[idx], combined_public_key[idx + 1]);
         let res = mapping.get(&scheme_id);
-        match res{
-            Ok(scheme_info)=>{
-                idx += 2;
-                let mut pk_length: usize = 0;
-                if matches!(scheme_info.pk_size_info.kind, SizeKind::VariableSized) {
-                    let pk_len_byte_size = scheme_info
-                        .pk_size_info
-                        .variable_size_bytelen
-                        .unwrap() as usize;
-                    let slice = &combined_public_key[idx..idx + pk_len_byte_size];
-                    idx += pk_len_byte_size;
-                    let mut vec_bytes = slice.to_vec();
-                    vec_bytes.resize(mem::size_of::<usize>(), 0u8);
-                    pk_length = usize::from_le_bytes(vec_bytes[..].try_into().unwrap());
-                }
-                else if matches!(scheme_info.pk_size_info.kind, SizeKind::FixedSized) {
-                    pk_length = scheme_info.pk_size_info.fixed_size.expect(format!("fixed_size for algorithm with id {} and config {} None while FixedSized is defined!", scheme_id.0, scheme_id.1).as_str()) as usize;
-                }
+        if res.is_err(){
+            return Err(format!(
+                "Algorithm with id {} and config {} not found!",
+                scheme_id.0, scheme_id.1
+            ))
+        }
+        let scheme_info = res.unwrap();
+        idx += 2;
+        let mut pk_length: usize = 0;
+        if matches!(scheme_info.pk_size_info.kind, SizeKind::VariableSized) {
+            let pk_len_byte_size = scheme_info
+                .pk_size_info
+                .variable_size_bytelen
+                .unwrap() as usize;
+            let slice = &combined_public_key[idx..idx + pk_len_byte_size];
+            idx += pk_len_byte_size;
+            let mut vec_bytes = slice.to_vec();
+            vec_bytes.resize(mem::size_of::<usize>(), 0u8);
+            pk_length = usize::from_le_bytes(vec_bytes[..].try_into().unwrap());
+        }
+        else if matches!(scheme_info.pk_size_info.kind, SizeKind::FixedSized) {
+            pk_length = scheme_info.pk_size_info.fixed_size.expect(format!("fixed_size for algorithm with id {} and config {} None while FixedSized is defined!", scheme_id.0, scheme_id.1).as_str()) as usize;
+        }
 
-                let pk = &combined_public_key[idx..idx + pk_length];
-                id_mapping.insert(scheme_id, pk.to_vec());
-                idx += pk_length;
-                Ok(return ParsedCombinedPublicKey {
-                    id_mapping: id_mapping
-                })
-                }
-                Err(_)=>{
-                    Err(format!(
-                        "Algorithm with id {} and config {} not found!",
-                        scheme_id.0, scheme_id.1
-                    ))
-                }
-                
-            }
-            
-        }       
+        let pk = &combined_public_key[idx..idx + pk_length];
+        id_mapping.insert(scheme_id, pk.to_vec());
+        idx += pk_length;
+    }
+    Ok(return ParsedCombinedPublicKey {
+        id_mapping: id_mapping
+    })
 }
